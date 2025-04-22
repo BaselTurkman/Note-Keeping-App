78dd241e8f91552ddc4aeb2b7a79d3abde33fdb6
BaselTurkman <Basel.Turkman123@gmail.com>
2025-04-22 11:53:49 +0300

    Setup React with Mui

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+import {Box} from "@mui/material"
+
+
+function App() {
+  
+  return (
+    <Box>
+      Hello 
+    </Box>
+  )
+}
+
+export default App;
